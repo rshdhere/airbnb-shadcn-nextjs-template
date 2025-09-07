f0b1c66fe8c7d2c34da7a379413538867f03c2d2
raashed <mohammedraashed10@gmail.com>
2025-09-07 14:08:37 +0530

    chore: lint-staged test successful

--- app/components/component.tsx
+++ app/components/component.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import cn from '@/lib/utils';
 
-const New = ({
+const ComponentX = ({
   className,
   children,
   ...props
@@ -14,7 +14,51 @@ const New = ({
       {...props}
       htmlFor="something"
       className={cn(
-        'm-12 rounded border-2 border-blue-500 bg-blue-50 p-10 text-blue-900',
+        'm-4 rounded border border-gray-300 bg-white p-6 text-gray-800 shadow-sm',
+        className,
+      )}
+    >
+      {children}
+    </label>
+  );
+};
+
+const ComponentY = ({
+  className,
+  children,
+  ...props
+}: {
+  className?: string;
+  children: React.ReactNode;
+} & React.LabelHTMLAttributes<HTMLLabelElement>) => {
+  return (
+    <label
+      {...props}
+      htmlFor="something"
+      className={cn(
+        'm-4 rounded border border-gray-300 bg-white p-6 text-gray-800 shadow-sm',
+        className,
+      )}
+    >
+      {children}
+    </label>
+  );
+};
+
+const ComponentZ = ({
+  className,
+  children,
+  ...props
+}: {
+  className?: string;
+  children: React.ReactNode;
+} & React.LabelHTMLAttributes<HTMLLabelElement>) => {
+  return (
+    <label
+      {...props}
+      htmlFor="something"
+      className={cn(
+        'm-4 rounded border border-gray-300 bg-white p-6 text-gray-800 shadow-sm',
         className,
       )}
     >
@@ -25,8 +69,10 @@ const New = ({
 
 export default function Component() {
   return (
-    <div>
-      <New>hii there</New>
+    <div className="space-y-4">
+      <ComponentX>First component</ComponentX>
+      <ComponentY>Second componet</ComponentY>
+      <ComponentZ>Third componet</ComponentZ>
     </div>
   );
 }
